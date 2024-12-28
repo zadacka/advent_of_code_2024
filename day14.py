c638de63e449c4ea22ccd3f30dfa7160551de62a
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2024-12-28 20:35:14 +0000

    day 14 part 2

--- day14.py
+++ day14.py
@@ -18,18 +18,22 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3"""
 
+
 # test_input = """\
 # p=2,4 v=2,-3"""
 @dataclasses.dataclass
 class Robot:
-    x:int
-    y:int
-    vx:int
-    vy:int
+    x: int
+    y: int
+    vx: int
+    vy: int
+
 
 # BOARD_WIDTH = 11
 BOARD_WIDTH = 101
 BOARD_HEIGHT = 103
+
+
 # BOARD_HEIGHT = 7
 
 
@@ -43,8 +47,30 @@ def print_board(robots):
         print('')
 
 
+def looks_like_a_christmas_tree(robots):
+    # let's assume there are some straight lines
+    col_population = [[] for col in range(BOARD_WIDTH)]
+    for robot in robots:
+        col_population[robot.x].append(robot.y)
+
+    max_col = 0
+    previous = 0
+    for pop in col_population:
+        height = 0
+        for y in sorted(pop):
+            if y == previous + 1:
+                height += 1
+            else:
+                max_col = max(height, max_col)
+                height = 0
+            previous = y
+    if max_col > 5:
+        return True
+    return False
+
+
 if __name__ == '__main__':
-    input_string = test_input
+    # input_string = test_input
     input_string = open("input14.txt").read()
 
     robots = []
@@ -54,10 +80,9 @@ if __name__ == '__main__':
             Robot(int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)))
         )
     print(robots)
-    seconds_left = 100
-    while seconds_left:
-        # print_board(robots)
-
+    seconds_elapsed = 0
+    # while seconds_left:
+    while True:
         for robot in robots:
             # print(f"Current point: {robot.x}, {robot.y}, moving {robot.vx}, {robot.vy}")
             robot.x += robot.vx
@@ -71,11 +96,13 @@ if __name__ == '__main__':
             while robot.y < 0:
                 robot.y += BOARD_HEIGHT
             # print(f"-> to {robot.x}, {robot.y}")
-        seconds_left -= 1
+        seconds_elapsed += 1
+        if looks_like_a_christmas_tree(robots):
+            print(f"Seconds elapsed: {seconds_elapsed}")
+            print_board(robots)
+            break
+    quadrants = [0, 0, 0, 0]
 
-    quadrants = [0,0,0,0]
-    # for r in range(BOARD_HEIGHT):
-    #     for c in range(BOARD_WIDTH):
     for robot in robots:
         if robot.y < BOARD_HEIGHT // 2:
             if robot.x < BOARD_WIDTH // 2:
