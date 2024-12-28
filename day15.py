9fb9c93c3da3be0a2068922504a8ed45194746f4
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2024-12-28 22:46:49 +0000

    day 15 with a lot of help from HyperNeutrino ... but where was I going wrong?!

--- day15.py
+++ day15.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 small_example = """\
 ########
 #..O.O.#
@@ -35,115 +33,56 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
 
-
-def parse_input(input_str):
-    grid = defaultdict(str)
-    moves = []
-
-    for y, line in enumerate(input_str.splitlines()):
-        if line.startswith('#'):
-            for x, char in enumerate(line):
-                grid[(x, y)] = char
-        else:
-            for char in line:
-                moves.append(char)
-
-    return grid, moves
-
-
-def print_grid(grid):
-    result = ''
-    x_max, y_max = max(grid.keys())
-    for y in range(y_max + 1):
-        for x in range(x_max + 1):
-            result += (grid[(x, y)])
-        result += '\n'
-    print(result)
-
-
-def do_move(grid, move):
-    max_row, max_col = max(grid.keys())
-
-    robotx, roboty = next((key for key, val in grid.items() if val == '@'), None)
-    barrels_to_move = []
-    if move == '<':
-        for col in range(robotx - 1, 0, -1):
-            if grid[col, roboty] == 'O':
-                barrels_to_move.append((col, roboty))
-            elif grid[col, roboty] == '.':
-                for barrel in barrels_to_move:
-                    barrelx, barrely = barrel
-                    grid[barrelx - 1, barrely] = 'O'
-                grid[robotx, roboty] = '.'
-                grid[robotx-1, roboty] = '@'
-                break
-            elif grid[col, roboty] == '#':
-                break
-    if move == '>':
-        for col in range(robotx + 1, max_col):
-            if grid[col, roboty] == 'O':
-                barrels_to_move.append((col, roboty))
-            elif grid[col, roboty] == '.':
-                for barrel in barrels_to_move:
-                    barrelx, barrely = barrel
-                    grid[barrelx + 1, barrely] = 'O'
-                grid[robotx, roboty] = '.'
-                grid[robotx+1, roboty] = '@'
-                break
-    if move == '^':
-        for row in range(roboty - 1, 0, -1):
-            item = grid[robotx, row]
-            if item == 'O':
-                barrels_to_move.append((robotx, row))
-            elif item == '.':
-                for barrel in barrels_to_move:
-                    barrelx, barrely = barrel
-                    grid[barrelx, barrely - 1] = 'O'
-                grid[robotx, roboty] = '.'
-                grid[robotx, roboty - 1] = '@'
-                break
-            else:
-                break # probably hit the edge
-    if move == 'v':
-        for row in range(roboty + 1, max_row):
-            item = grid[robotx, row]
-            if item == 'O':
-                barrels_to_move.append((robotx, row))
-            elif item == '.':
-                for barrel in barrels_to_move:
-                    barrelx, barrely = barrel
-                    grid[barrelx, barrely + 1] = 'O'
-                grid[robotx, roboty] = '.'
-                grid[robotx, roboty + 1] = '@'
-                break
-            else:
-                break # probably hit the edge
-    return grid
-
-
-def calculate_checksum(grid):
-    result = 0
-    for pt, value in grid.items():
-        if value == 'O':
-            x,y = pt
-            result += 100 * y + x
-    return result
-
 if __name__ == '__main__':
-    # grid, moves = parse_input(small_example)
-    grid, moves = parse_input(test_input)
-    # grid, moves = parse_input(open("input15.txt").read())
+    # input_str = test_input
+    input_str = open("input15.txt").read()
 
-    # print(f"Initial state:")
-    # print_grid(grid)
+    top, bottom = input_str.split('\n\n')
+    grid = list([char for char in line] for line in top.splitlines())
+    moves = bottom.replace("\n", "")
 
+    rows = len(grid)
+    cols = len(grid[0])
+
+    robot = None
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '@':
+                robot = (r, c)
+
+    r, c = robot
     for move in moves:
-        grid = do_move(grid, move)
-        # print(f"Move {move}:")
-        # print_grid(grid)
+        dr = {"^": -1, "v": 1}.get(move, 0)
+        dc = {"<": -1, ">": 1}.get(move, 0)
+        cr = r
+        cc = c
+        go = True
+        targets = [(r, c)]
+        while True:
+            cr += dr
+            cc += dc
+            char = grid[cr][cc]
+            if char == '#':
+                go = False
+                break
+            if char == 'O':
+                targets.append((cr, cc))
+            if char == '.':
+                break
+        if go:
+            grid[r][c] = '.'  # robot moved!
+            grid[r + dr][c + dc] = '@'  # robot moved!
+            for br, bc in targets[1:]:
+                grid[br + dr][bc + dc] = 'O'
+            r += dr
+            c += dc
+    print("---------------")
+    for row in grid:
+        print(*row, sep='')
 
-    checksum = calculate_checksum(grid)
-    print(f"Checksum: {checksum}")
-
-    # smaller: 2028
-    # medium: 10092.
+    checksum = sum(100 * r + c
+                   for r in range(rows)
+                   for c in range(cols)
+                   if grid[r][c] == 'O'
+                   )
+    print(checksum)
