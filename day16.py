7d30a6e1d46123eedd617b59722423e249042a6d
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2024-12-29 22:02:42 +0000

    Day 16 part 2 ... broken by 'best route' restriction (facepalm)
    .. but if I undo that, then it takes forever....
    
    Back to the drawing board, Dijkstra here we come...

--- day16.py
+++ day16.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 test_input = """\
 ###############
 #.......#....E#
@@ -55,26 +57,32 @@ if __name__ == '__main__':
     stack = []  # position, direction, visited, score
     scores = []
 
-    best_route = dict()
+    # best_route = dict()
+    routes = defaultdict(list)
 
     (r, c), direction, visited, score = start, 'E', set(), 0
     try_popstack = False
     while True:
 
-        if (r, c) not in best_route:
-            best_route[(r, c)] = score
-        elif score < best_route[(r, c)]:
-            best_route[(r, c)] = score
-        else:
-            try_popstack = True
+        # if (r, c) not in best_route:
+        #     best_route[(r, c)] = score
+        # elif score < best_route[(r, c)]:
+        #     best_route[(r, c)] = score
+        # else:
+        #     try_popstack = True
+        visited.add((r, c))
 
         if gridmap[(r, c)] == 'E':
             print(f"Made it! {score}")
             scores.append(score)
+            routes[score].append(visited)
             try_popstack = True
 
         if gridmap[(r, c)] == '#':
             try_popstack = True
+
+        if score > 102504:
+            try_popstack = True  # hack because we know the best route!
 
         if try_popstack:
             if stack:
@@ -84,8 +92,8 @@ if __name__ == '__main__':
             else:
                 break
 
-        print(r, c)
-        visited.add((r, c))
+        # print(r, c)
+        # visited.add((r, c))
 
         lrf = forward_left_right[direction]
 
@@ -101,3 +109,23 @@ if __name__ == '__main__':
         score += 1
 
     print(sorted(scores))
+    best_route_score = sorted(scores)[0]
+    print(f"Best route score: {best_route_score}")
+    best_routes = routes[best_route_score]
+    # print(best_routes)
+
+    good_seats = set()
+    for route in best_routes:
+        for pt in route:
+            good_seats.add(pt)
+
+    print(len(good_seats))
+
+    start = None
+    for r, line in enumerate(grid):
+        for c, char in enumerate(line):
+            if (r, c) in good_seats:
+                print ('O', end='')
+            else:
+                print(gridmap[(r, c)], end='')
+        print('\n', end='')
