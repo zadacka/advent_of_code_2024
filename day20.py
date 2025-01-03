721a64954485d13b5b5bc2cb16d676d3e6219580
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2025-01-03 21:29:57 +0000

    Day 20 almost part 1
    (With a HN approach... was the problem in mine just the LEQ issue?! boo!)

--- day20.py
+++ day20.py
@@ -1,4 +1,4 @@
-from collections import deque, defaultdict
+from collections import deque
 
 test_input = """\
 ###############
@@ -18,72 +18,43 @@ test_input = """\
 ###############"""
 
 if __name__ == '__main__':
-    input_str = test_input
+    # input_str = test_input
     input_str = open("input20.txt").read()
 
-    grid = [list(row) for row in input_str.split('\n')]
-    start = None
-    end = None
-    track = set()
-    for r, row in enumerate(grid):
-        for c, char in enumerate(row):
-            if char == 'S':
-                start = (r, c)
-            elif char == 'E':
-                end = (r, c)
-            if char in 'S.E':
-                track.add((r, c))
+    grid = [list(row) for row in input_str.splitlines()]
 
-    seen = {start: 0}
-    q = deque([(*start, 0)])
-    full_path = None
+    rows = len(grid)
+    cols = len(grid[0])
 
-    while q:
-        r, c, d = q.popleft()
-        # print(r, c)
-        if (r, c) == end:
-            print(f"best path: {d}")
-            full_path = d
-            break
-        for nr, nc in (r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1):
-            if (nr, nc) in track and (nr, nc) not in seen:
-                q.append((nr, nc, d + 1))
-                seen[(nr, nc)] = d + 1
-
-    distance_from_end = {k: d - v for k, v in seen.items()}
-
-    pts = track - set(distance_from_end.keys())
-    for tr, tc in pts:
-        if (tr, tc) in distance_from_end:
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 'S':
+                break  # r, c values preserved
+        else:
             continue
+        break
 
-        q = deque([(tr, tc, 0)])
-        seen2 = {(tr, tc): 0}
-        while q:
-            r, c, d = q.popleft()
-            if (r, c) in distance_from_end:
-                for pt, d in seen2.items():
-                    distance_from_end[pt] = d + distance_from_end[(r, c)]
-            for nr, nc in (r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1):
-                if (nr, nc) in track and (nr, nc) not in seen:
-                    q.append((nr, nc, d + 1))
-                    seen2[(nr, nc)] = d + 1
-    print(distance_from_end)
+    dists = [[-1] * cols for _ in range(rows)]
+    q = deque([(r, c)])
+    dists[r][c] = 0
+    while q:
+        r, c = q.popleft()
+        for nr, nc in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+            if nr < 0 or nc < 0 or nr >= rows or nc >= cols: continue
+            if dists[nr][nc] != -1: continue  # already better path
+            if grid[nr][nc] == '#': continue  # wall
+            dists[nr][nc] = dists[r][c] + 1
+            q.append((nr, nc))
+    # for row in dists:
+    #     print(*row, sep='\t')
 
-    saved = defaultdict(int)
-    for (r, c), distance in seen.items():
-        for nr, nc in (r + 2, c), (r - 2, c), (r, c - 2), (r, c + 2):
-            if (nr, nc) in track:
-                new_path_time = distance + distance_from_end[(nr, nc)] + 2
-                if new_path_time < full_path:
-                    saved[full_path - new_path_time] += 1
-                    # print(f"Found a short cut!")
-    for k in sorted(saved):
-        print(f"Found {saved[k]} paths that saved {k} picoseconds")
-
-    great_cheats = 0
-    for k, v in saved.items():
-        if v > 100:
-            great_cheats += k
-
-    print(f"Great cheats: {great_cheats}")
+    count = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '#': continue
+            for nr, nc in (r + 2, c), (r + 1, c + 1), (r, c + 2), (r - 1, c + 1):
+                if nr < 0 or nc < 0 or nr >= rows or nc >= cols: continue
+                if grid[nr][nc] == '#': continue
+                if abs(dists[r][c] - dists[nr][nc]) >= 102:
+                    count += 1
+    print(count)
