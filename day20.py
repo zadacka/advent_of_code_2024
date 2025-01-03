3dc15c1c98657ca5308470aabc385633b05da15d
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2025-01-03 21:58:19 +0000

    Day 20 part 2 - slowish but simple quadratic solution!

--- day20.py
+++ day20.py
@@ -26,16 +26,18 @@ if __name__ == '__main__':
     rows = len(grid)
     cols = len(grid[0])
 
+    track = set()
+    start = None
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 'S':
-                break  # r, c values preserved
-        else:
-            continue
-        break
+                start = r, c
+            if grid[r][c] in "SE.":
+                track.add((r, c))
 
     dists = [[-1] * cols for _ in range(rows)]
-    q = deque([(r, c)])
+    q = deque([start])
+    r, c = start
     dists[r][c] = 0
     while q:
         r, c = q.popleft()
@@ -49,12 +51,10 @@ if __name__ == '__main__':
     #     print(*row, sep='\t')
 
     count = 0
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == '#': continue
-            for nr, nc in (r + 2, c), (r + 1, c + 1), (r, c + 2), (r - 1, c + 1):
-                if nr < 0 or nc < 0 or nr >= rows or nc >= cols: continue
-                if grid[nr][nc] == '#': continue
-                if abs(dists[r][c] - dists[nr][nc]) >= 102:
-                    count += 1
+    for (r, c) in track:
+        for (nr, nc) in track:
+            shortcut_cost = abs(nr - r) + abs(nc - c)
+            saving = dists[nr][nc] - (dists[r][c] + shortcut_cost)
+            if shortcut_cost <= 20 and saving >= 100:
+                count += 1
     print(count)
