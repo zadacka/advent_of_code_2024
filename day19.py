9df19e628a6d9c6eb325431ba7db9f9f6318cee8
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2025-01-03 19:48:31 +0000

    Day 19 part 2
    HyperNeutrino again - nice recursive solution to part 2.

--- day19.py
+++ day19.py
@@ -1,5 +1,7 @@
 from collections import deque
 
+from functools import cache
+
 test_input = """\
 r, wr, b, g, bwu, rb, gb, br
 
@@ -38,10 +40,28 @@ if __name__ == '__main__':
     # input_str = test_input
     input_str = open("input19.txt").read()
 
-    available_towels = [pattern.strip() for pattern in input_str.splitlines()[0].split(',')]
-    target_patterns = [target.strip() for target in input_str.splitlines()[2:]]
-
-    viable_patterns = calculate_arrangements(available_towels, target_patterns, find_first=False)
-    print(viable_patterns)
+    towels = [pattern.strip() for pattern in input_str.splitlines()[0].split(',')]
+    targets = [target.strip() for target in input_str.splitlines()[2:]]
 
 
+    @cache
+    def can_find(design):
+        if design == '': return True
+        for i in range(len(design) + 1):
+            if design[:i] in towels and can_find(design[i:]):
+                return True
+        return False
+
+
+    @cache
+    def num_possibilities(design):
+        if design == '': return 1
+        count = 0
+        for i in range(len(design) + 1):
+            if design[:i] in towels:
+                count += num_possibilities(design[i:])
+        return count
+
+
+    print(sum(can_find(d) for d in targets))
+    print(sum(num_possibilities(d) for d in targets))
