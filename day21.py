2f70fd293e4ec9ecc17f421d1cc8416aec418a28
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2025-01-12 12:12:24 +0000

    Day 21 part 2 - recursive (depth first) + caching

--- day21.py
+++ day21.py
@@ -7,10 +7,12 @@
 # +---+---+---+
 #     | 0 | A |
 #     +---+---+
-from itertools import product
+from collections import deque
+from functools import cache
 
 import math
-from collections import deque
+from itertools import product
+
 
 #     +---+---+
 #     | ^ | A |
@@ -18,20 +20,7 @@ from collections import deque
 # | < | v | > |
 # +---+---+---+
 
-numpad = [
-    ["7", "8", "9"],
-    ["4", "5", "6"],
-    ["1", "2", "3"],
-    [None, "0", "A"]
-]
-
-dirpad = [
-    [None, "^", "A"],
-    ["<", "v", ">"]
-]
-
-
-def solve(steps, keypad):
+def compute_sequences(keypad):
     rows, cols = len(keypad), len(keypad[0])
     key2loc = {}
     allowed_positions = set()
@@ -53,7 +42,7 @@ def solve(steps, keypad):
             optimal = math.inf
             while q:
                 (r, c), moves = q.popleft()
-                for nr, nc, nm in (r-1, c, "^"), (r+1, c, "v"), (r, c-1, "<"), (r, c+1, ">"):
+                for nr, nc, nm in (r - 1, c, "^"), (r + 1, c, "v"), (r, c - 1, "<"), (r, c + 1, ">"):
                     if (nr, nc) not in allowed_positions: continue
                     if (nr, nc) == key2loc[y]:
                         if len(moves) + 1 > optimal:
@@ -66,11 +55,42 @@ def solve(steps, keypad):
                     continue
                 break
             paths[(x, y)] = possible_paths
+    return paths
 
+
+numpad = [
+    ["7", "8", "9"],
+    ["4", "5", "6"],
+    ["1", "2", "3"],
+    [None, "0", "A"]
+]
+numpad_paths = compute_sequences(numpad)
+
+dirpad = [
+    [None, "^", "A"],
+    ["<", "v", ">"]
+]
+dirpad_paths = compute_sequences(dirpad)
+dirpad_path_lengths = {k: len(v[0]) for k, v in dirpad_paths.items()}
+
+
+def solve(steps, keypad_paths):
     result = []
     for first, second in zip("A" + steps, steps):
-        result.append(paths[first, second])
+        result.append(keypad_paths[first, second])
     return [''.join(x) for x in product(*result)]
+
+@cache
+def comput_length(x, y, depth=2):
+    if depth == 1:
+        return dirpad_path_lengths[x, y]
+    optimal = math.inf
+    for path in dirpad_paths[x, y]:
+        length = 0
+        for a, b in zip("A" + path, path):
+            length += comput_length(a, b, depth - 1)
+        optimal = min(optimal, length)
+    return optimal
 
 test_input = """\
 029A
@@ -81,28 +101,21 @@ test_input = """\
 
 if __name__ == '__main__':
     # input_string = test_input
-    input_string= open("input21.txt").read()
+    input_string = open("input21.txt").read()
 
     total = 0
     for line in input_string.splitlines():
-        robot1_options = solve(line, numpad)
+        inputs = solve(line, numpad_paths)
 
-        robot2_options = []
-        for path in robot1_options:
-            robot2_options.extend(solve(path, dirpad))
-        optimal = min(map(len, robot2_options))
-        robot2_options = [r for r in robot2_options if len(r) == optimal]
-        # print(robot2_options)
-
-        robot3_options = []
-        for path in robot2_options:
-            robot3_options.extend(solve(path, dirpad))
-        optimal = min(map(len, robot3_options))
-        robot3_options = [r for r in robot3_options if len(r) == optimal]
-        # print("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A" in robot3_options)
-        complexity = len(robot3_options[0])
+        optimal = math.inf
+        for path in inputs:
+            length = 0
+            for a, b in zip("A" + path, path):
+                length += comput_length(a, b, depth=25)
+            optimal = min(optimal, length)
+        print(optimal)
         numeric = int(line[:-1])
-        result = complexity * numeric
-        print(f"{complexity}x{numeric} = {result}")
+        result = optimal * numeric
+        print(f"{optimal}x{numeric} = {result}")
         total += result
     print(total)
