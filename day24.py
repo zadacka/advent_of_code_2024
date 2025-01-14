79366e106c52537e9d881edb10066ff8a8ab8c3e
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2025-01-14 21:59:18 +0000

    Day 24 part 2 - yikes, just FOLLOWING ALONG was hard for this one!!
    Think through how an adder works with AND/OR/XOR gates (nice!)
    Think about the operations that need to be in place for each increasing z* output wire
    Verify each increasing z* output wire based on the required adder connection types
    
    HyperNeutrino rockets through this once again, with awesome commentary, making it seem
     feasible rather than the head scratcher it initially first appears as

--- day24.py
+++ day24.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 small_test_input = """\
 x00: 1
 x01: 1
@@ -61,6 +59,29 @@ hwm AND bqk -> z03
 tgd XOR rvg -> z12
 tnw OR pbm -> gnj"""
 
+
+def calc_answer(wires, gates):
+    while gates:
+        input1, input2, gate_type, output = gates.popleft()
+        if input1 in wires and input2 in wires:
+            # print(f"{input1} and {input2} values known! can calculate {output}")
+            wires[output] = functions[gate_type](wires[input1], wires[input2])
+
+        else:
+            # print(f"{input1} or {input2} values not yet known, so cannot calculate {output}")
+            gates.append((input1, input2, gate_type, output))
+    wire_values = tuple((wire, value) for wire, value in wires.items() if wire.startswith('z'))
+    wire_values = sorted(wire_values, reverse=True)
+    binary_value = ''.join([str(v) for w, v in wire_values if w.startswith('z')])
+    print(int(binary_value, base=2))
+
+
+functions = {
+    'AND': lambda x, y: x & y,
+    'OR': lambda x, y: x | y,
+    'XOR': lambda x, y: x ^ y,
+}
+
 if __name__ == '__main__':
     # input_str = test_input
     input_str = open('input24.txt').read()
@@ -71,29 +92,85 @@ if __name__ == '__main__':
         for wire in wires.splitlines()
         for name, value in [wire.split(': ')]  # this is magical - list of length 1 :)
     }
-    gates = deque([
-        (input1, input2, gate_type, output)
+    gates = {
+        z: (op, x, y)
         for gate in gates.splitlines()
-        for inputs, output in [gate.split(' -> ')]
-        for input1, gate_type, input2 in [inputs.split()]
-    ])
-
-    functions = {
-        'AND': lambda x, y: x and y,
-        'OR': lambda x, y: x or y,
-        'XOR': lambda x, y: x ^ y,
+        for x, op, y, z in [gate.replace(' -> ', ' ').split()]
     }
-    while gates:
-        input1, input2, gate_type, output = gates.popleft()
-        if input1 in wires and input2 in wires:
-            print(f"{input1} and {input2} values known! can calculate {output}")
-            wires[output] = functions[gate_type](wires[input1], wires[input2])
 
-        else:
-            print(f"{input1} or {input2} values not yet known, so cannot calculate {output}")
-            gates.append((input1, input2, gate_type, output))
 
-    wire_values = tuple((wire, value) for wire, value in wires.items() if wire.startswith('z'))
-    wire_values = sorted(wire_values, reverse=True)
-    binary_value = ''.join([str(v) for w, v in wire_values if w.startswith('z')])
-    print(int(binary_value, base=2))
+    def verify_z(wire, num):
+        # print(f"vz {wire} {num}")
+        if wire not in gates: return False
+        op, x, y = gates[wire]
+        if op != 'XOR': return False
+        if num == 0: return sorted([x, y]) == ['x00', 'y00']
+        return verify_intermediate_xor(x, num) and verify_carry_bit(y, num) or verify_intermediate_xor(y,
+                                                                                                       num) and verify_carry_bit(
+            x, num)
+
+
+    def verify_intermediate_xor(wire, num):
+        # print(f"vx {wire} {num}")
+        if wire not in gates: return False
+        op, x, y = gates[wire]
+        if op != 'XOR': return False
+        return sorted([x, y]) == [f"x{num:0>2}", f"y{num:0>2}"]
+
+
+    def verify_carry_bit(wire, num):
+        # print(f"vc {wire} {num}")
+        if wire not in gates: return False
+        op, x, y = gates[wire]
+        if num == 1:
+            if op != 'AND': return False
+            return sorted([x, y]) == ["x00", "y00"]
+        if op != 'OR': return False
+        return verify_direct_carry(x, num - 1) and verify_recarry(y, num - 1) or verify_direct_carry(y,
+                                                                                                     num - 1) and verify_recarry(
+            x, num - 1)
+
+
+    def verify_direct_carry(wire, num):
+        # print(f"vd {wire} {num}")
+        if wire not in gates: return False
+        op, x, y = gates[wire]
+        if op != 'AND': return False
+        return sorted([x, y]) == [f"x{num:0>2}", f"y{num:0>2}"]
+
+
+    def verify_recarry(wire, num):
+        # print(f"vr {wire} {num}")
+        if wire not in gates: return False
+        op, x, y = gates[wire]
+        if op != 'AND': return False
+        return verify_intermediate_xor(x, num) and verify_carry_bit(y, num) or verify_intermediate_xor(y,
+                                                                                                       num) and verify_carry_bit(
+            x, num)
+
+
+    def verify(num):
+        return verify_z(f"z{num:0>2}", num)
+
+    def progress():
+        i = 0
+        while True:
+            if not verify(i): break
+            i += 1
+        return i
+
+    result = []
+    for _ in range(4):
+        baseline = progress()
+        for x in gates:
+            for y in gates:
+                if x == y: continue
+                gates[x], gates[y] = gates[y], gates[x]
+                if progress() > baseline:
+                    break
+                gates[x], gates[y] = gates[y], gates[x]
+            else:
+                continue
+            break
+        result.extend([x, y])
+    print(','.join(sorted(result)))
